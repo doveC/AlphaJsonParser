dc0c99fa71b3fa96fca6b99f6df05fd81742449c
doveC <laihaodongc@gmail.com>
2024-03-18 01:07:34 +0800

    Format code and add README file
    - Applied code formatting for better readability and consistency
    - Added README.md to provide project overview, setup instructions, and usage examples

--- JsonParser.h
+++ JsonParser.h
@@ -1,13 +1,13 @@
+#include <fstream>
 #include <iostream>
-#include <variant>
-#include <vector>
 #include <map>
 #include <optional>
-#include <string>
-#include <fstream>
 #include <sstream>
+#include <string>
+#include <variant>
+#include <vector>
 
-namespace json 
+namespace json
 {
 
 struct Node;
@@ -20,26 +20,26 @@ using Array = std::vector<Node>;
 using Object = std::map<std::string, Node>;
 using Value = std::variant<Null, Bool, Int, Float, String, Array, Object>;
 
-struct Node 
+struct Node
 {
     Value value;
 
     Node() : value(Null{}) {}
     Node(Value _value) : value(_value) {}
 
-    auto& operator[](const std::string& key) 
+    auto& operator[](const std::string& key)
     {
-        if (auto object = std::get_if<Object>(&value)) 
+        if (auto object = std::get_if<Object>(&value))
         {
-            return  (*object)[key];
+            return (*object)[key];
         }
 
         throw std::runtime_error("not an object");
     }
 
-    auto& operator[](size_t index) 
+    auto& operator[](size_t index)
     {
-        if (auto array = std::get_if<Array>(&value)) 
+        if (auto array = std::get_if<Array>(&value))
         {
             return array->at(index);
         }
@@ -47,21 +47,21 @@ struct Node
         throw std::runtime_error("not an array");
     }
 
-    void push(const Node& rhs) 
+    void push(const Node& rhs)
     {
-        if (auto array = std::get_if<Array>(&value)) 
+        if (auto array = std::get_if<Array>(&value))
         {
             array->push_back(rhs);
         }
     }
 };
 
-struct JsonParser 
+struct JsonParser
 {
     std::string_view json_str;
     size_t pos = 0;
 
-    void parseWhiteSpace() 
+    void parseWhiteSpace()
     {
         while (pos < json_str.size() && std::isspace(json_str[pos]))
         {
@@ -69,20 +69,20 @@ struct JsonParser
         }
     }
 
-    auto parseNull() -> std::optional<Value> 
+    auto parseNull() -> std::optional<Value>
     {
-        if (json_str.substr(pos, 4) == "null") 
+        if (json_str.substr(pos, 4) == "null")
         {
             pos += 4;
             return Null{};
         }
 
-        return{};
+        return {};
     }
 
-    auto parseTrue() -> std::optional<Value> 
+    auto parseTrue() -> std::optional<Value>
     {
-        if (json_str.substr(pos, 4) == "true") 
+        if (json_str.substr(pos, 4) == "true")
         {
             pos += 4;
             return true;
@@ -91,9 +91,9 @@ struct JsonParser
         return {};
     }
 
-    auto parseFalse() -> std::optional<Value> 
+    auto parseFalse() -> std::optional<Value>
     {
-        if (json_str.substr(pos, 5) == "false") 
+        if (json_str.substr(pos, 5) == "false")
         {
             pos += 5;
             return false;
@@ -102,14 +102,12 @@ struct JsonParser
         return {};
     }
 
-    auto parseNumber() -> std::optional<Value> 
+    auto parseNumber() -> std::optional<Value>
     {
         size_t endpos = pos;
 
-        while (endpos < json_str.size() && (
-            std::isdigit(json_str[endpos]) ||
-            json_str[endpos] == 'e' ||
-            json_str[endpos] == '.')) 
+        while (endpos < json_str.size() &&
+               (std::isdigit(json_str[endpos]) || json_str[endpos] == 'e' || json_str[endpos] == '.'))
         {
             endpos++;
         }
@@ -117,42 +115,41 @@ struct JsonParser
         std::string number(json_str.substr(pos, endpos - pos));
         pos = endpos;
 
-        static auto isFloat = [](std::string& number) {
-            return number.find('.') != number.npos || number.find('e') != number.npos;
-        };
+        static auto isFloat = [](std::string& number)
+        { return number.find('.') != number.npos || number.find('e') != number.npos; };
 
-        if (isFloat(number)) 
+        if (isFloat(number))
         {
-            try 
+            try
             {
                 Float ret = std::stod(number);
                 return ret;
             }
-            catch (...) 
+            catch (...)
             {
                 return {};
             }
         }
-        else 
+        else
         {
-            try 
+            try
             {
                 Int ret = std::stoi(number);
                 return ret;
             }
-            catch (...) 
+            catch (...)
             {
                 return {};
             }
         }
     }
 
-    auto parseString() -> std::optional<Value> 
+    auto parseString() -> std::optional<Value>
     {
         pos++;  // skip left "
 
         size_t endpos = pos;
-        while (pos < json_str.size() && json_str[endpos] != '"') 
+        while (pos < json_str.size() && json_str[endpos] != '"')
         {
             endpos++;
         }
@@ -163,17 +160,17 @@ struct JsonParser
         return str;
     }
 
-    auto parseArray() -> std::optional<Value> 
+    auto parseArray() -> std::optional<Value>
     {
         pos++;  // skip [
 
         Array arr;
-        while (pos < json_str.size() && json_str[pos] != ']') 
+        while (pos < json_str.size() && json_str[pos] != ']')
         {
             auto value = parseValue();
             arr.push_back(value.value());
             parseWhiteSpace();
-            if (pos < json_str.size() && json_str[pos] == ',') 
+            if (pos < json_str.size() && json_str[pos] == ',')
             {
                 pos++;  // skip ,
             }
@@ -184,21 +181,21 @@ struct JsonParser
         return arr;
     }
 
-    auto parseObject() -> std::optional<Value> 
+    auto parseObject() -> std::optional<Value>
     {
         pos++;  // skip {
 
         Object obj;
-        while (pos < json_str.size() && json_str[pos] != '}') 
+        while (pos < json_str.size() && json_str[pos] != '}')
         {
             auto key = parseValue();
-            if (!std::holds_alternative<String>(key.value())) 
+            if (!std::holds_alternative<String>(key.value()))
             {
                 return {};
             }
 
             parseWhiteSpace();
-            if (pos < json_str.size() && json_str[pos] == ':') 
+            if (pos < json_str.size() && json_str[pos] == ':')
             {
                 pos++;  // skip :
             }
@@ -208,44 +205,43 @@ struct JsonParser
             obj[std::get<String>(key.value())] = val.value();
 
             parseWhiteSpace();
-            if (pos < json_str.size() && json_str[pos] == ',') 
+            if (pos < json_str.size() && json_str[pos] == ',')
             {
                 pos++;  // skip ,
             }
             parseWhiteSpace();
         }
-        pos++;// }
+        pos++;  // }
         return obj;
-
     }
 
-    auto parseValue() ->std::optional<Value> 
+    auto parseValue() -> std::optional<Value>
     {
         parseWhiteSpace();
-        switch (json_str[pos]) 
+        switch (json_str[pos])
         {
-            case 'n':
-                return parseNull();
-            case 't':
-                return parseTrue();
-            case 'f':
-                return parseFalse();
-            case '"':
-                return parseString();
-            case '[':
-                return parseArray();
-            case '{':
-                return parseObject();
-            default:
-                return parseNumber();
+        case 'n':
+            return parseNull();
+        case 't':
+            return parseTrue();
+        case 'f':
+            return parseFalse();
+        case '"':
+            return parseString();
+        case '[':
+            return parseArray();
+        case '{':
+            return parseObject();
+        default:
+            return parseNumber();
         }
     }
 
-    auto parse() -> std::optional<Node> 
+    auto parse() -> std::optional<Node>
     {
         parseWhiteSpace();
         auto value = parseValue();
-        if (!value) 
+        if (!value)
         {
             return {};
         }
@@ -254,33 +250,34 @@ struct JsonParser
     }
 };
 
-auto parser(std::string_view json_str) -> std::optional<Node> 
+auto parser(std::string_view json_str) -> std::optional<Node>
 {
-    JsonParser p{ json_str };
+    JsonParser p{json_str};
     return p.parse();
 }
 
-class JsonGenerator 
+class JsonGenerator
 {
-public:
-    static auto generate(const Node& node) -> std::string {
+  public:
+    static auto generate(const Node& node) -> std::string
+    {
         return std::visit(
             [](auto&& arg) -> std::string {
                 using T = std::decay_t<decltype(arg)>;
 
-                if constexpr (std::is_same_v<T, Null>) 
+                if constexpr (std::is_same_v<T, Null>)
                 {
                     return "null";
                 }
-                else if constexpr (std::is_same_v<T, Bool>) 
+                else if constexpr (std::is_same_v<T, Bool>)
                 {
                     return arg ? "true" : "false";
                 }
-                else if constexpr (std::is_same_v<T, Int>) 
+                else if constexpr (std::is_same_v<T, Int>)
                 {
                     return std::to_string(arg);
                 }
-                else if constexpr (std::is_same_v<T, Float>) 
+                else if constexpr (std::is_same_v<T, Float>)
                 {
                     return std::to_string(arg);
                 }
@@ -288,18 +285,18 @@ public:
                 {
                     return generateString(arg);
                 }
-                else if constexpr (std::is_same_v<T, Array>) 
+                else if constexpr (std::is_same_v<T, Array>)
                 {
                     return generateArray(arg);
                 }
-                else if constexpr (std::is_same_v<T, Object>) 
+                else if constexpr (std::is_same_v<T, Object>)
                 {
                     return generateObject(arg);
                 }
             },
             node.value);
     }
-    static auto generateString(const String& str) -> std::string 
+    static auto generateString(const String& str) -> std::string
     {
         std::string json_str = "\"";
         json_str += str;
@@ -307,16 +304,16 @@ public:
 
         return json_str;
     }
-    static auto generateArray(const Array& array) -> std::string 
+    static auto generateArray(const Array& array) -> std::string
     {
         std::string json_str = "[";
-        for (const auto& node : array) 
+        for (const auto& node : array)
         {
             json_str += generate(node);
             json_str += ',';
         }
 
-        if (!array.empty()) 
+        if (!array.empty())
         {
             json_str.pop_back();
         }
@@ -324,11 +321,11 @@ public:
 
         return json_str;
     }
-    static auto generateObject(const Object& object) -> std::string 
+    static auto generateObject(const Object& object) -> std::string
     {
         std::string json_str = "{";
 
-        for (const auto& [key, node] : object) 
+        for (const auto& [key, node] : object)
         {
             json_str += generateString(key);
             json_str += ':';
@@ -348,7 +345,7 @@ public:
 
 class JsonGeneratorFormat
 {
-public:
+  public:
     static int currentIndentLevel;
 
     static auto generateToFormatString(const Node& node) -> std::string
@@ -369,19 +366,19 @@ public:
                     addIndent(str);
                 }
 
-                if constexpr (std::is_same_v<T, Null>) 
+                if constexpr (std::is_same_v<T, Null>)
                 {
                     str += "null";
                 }
-                else if constexpr (std::is_same_v<T, Bool>) 
+                else if constexpr (std::is_same_v<T, Bool>)
                 {
                     arg ? (str += "true") : (str += "false");
                 }
-                else if constexpr (std::is_same_v<T, Int>) 
+                else if constexpr (std::is_same_v<T, Int>)
                 {
                     str += std::to_string(arg);
                 }
-                else if constexpr (std::is_same_v<T, Float>) 
+                else if constexpr (std::is_same_v<T, Float>)
                 {
                     str += std::to_string(arg);
                 }
@@ -389,11 +386,11 @@ public:
                 {
                     str += generateString(arg);
                 }
-                else if constexpr (std::is_same_v<T, Array>) 
+                else if constexpr (std::is_same_v<T, Array>)
                 {
                     str += generateArray(arg);
                 }
-                else if constexpr (std::is_same_v<T, Object>) 
+                else if constexpr (std::is_same_v<T, Object>)
                 {
                     str += generateObject(arg);
                 }
@@ -402,7 +399,7 @@ public:
             },
             node.value);
     }
-    static auto generateString(const String& str) -> std::string 
+    static auto generateString(const String& str) -> std::string
     {
         std::string res = "\"";
         res += str;
@@ -410,10 +407,10 @@ public:
 
         return res;
     }
-    static auto generateArray(const Array& array) -> std::string 
+    static auto generateArray(const Array& array) -> std::string
     {
         std::string str;
-        
+
         str += "[\n";
         currentIndentLevel++;
         for (size_t i = 0; i < array.size(); i++)
@@ -432,10 +429,10 @@ public:
 
         return str;
     }
-    static auto generateObject(const Object& object) -> std::string 
+    static auto generateObject(const Object& object) -> std::string
     {
         std::string str;
-        
+
         str += "{\n";
         currentIndentLevel++;
         for (const auto& [key, node] : object)
@@ -470,10 +467,10 @@ public:
 };
 int JsonGeneratorFormat::currentIndentLevel = 0;
 
-auto operator<<(std::ostream& out, const Node& t) -> std::ostream& 
+auto operator<<(std::ostream& out, const Node& t) -> std::ostream&
 {
     out << JsonGeneratorFormat::generate(t);
 
     return out;
 }
-}
+}  // namespace json
